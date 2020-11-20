5059f3cc1510eac8526c118d2fd9c5113ae84162
Ildar1982 <68797569+Ildar1982@users.noreply.github.com>
2020-11-20 20:38:34 +0500

    init

---
+++ Assets/Scripts/Monstr.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monstr : MonoBehaviour
+{
+    private float _speed = 1;
+
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemy;
+    [SerializeField] private Transform[] _spawnPoints;
+    [SerializeField] private float _secondsBetweenSpawn;
+
+    private float _elapsedTime;
+    
+    void Update()
+    {
+        _elapsedTime += Time.deltaTime;        
+        if (_elapsedTime >= _secondsBetweenSpawn)
+        {
+            _elapsedTime = 0;
+            int numberPoints = Random.Range(0, _spawnPoints.Length);
+            Instantiate(_enemy, _spawnPoints[numberPoints]);
+        }
+    }
+}
